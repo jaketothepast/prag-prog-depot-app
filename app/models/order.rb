6b07ca4d3c237555130ca321fc09dc7c31a8f67a
jaketothepast <windlejacob12@gmail.com>
2024-04-19 10:15:03 -0400

    Fixing relationship between models to fix tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
   enum pay_type: {
          "Check" => 1,
          "Credit card" => 2,
          "Purchase order" => 3
        }
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
